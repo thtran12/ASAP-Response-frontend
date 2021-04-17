da109336f6e1cbc531be6f11223aa656a077ad77
Marysia <thtran12@gmail.com>
2021-04-17 15:59:26 -0700

    created LandingPage and added two functions to App.js

--- src/App.js
+++ src/App.js
@@ -13,6 +13,19 @@ const App = () => {
 		width: '100vw',
 		height: '100vh',
 	});
+
+  const convertToRadians = val => {return val/(180/Math.PI)};
+  const distanceInMiles = (lat1,long1,lat2,long2) => {
+    const lat1Rad = convertToRadians(lat1);
+    const long1Rad = convertToRadians(long1);
+    const lat2Rad = convertToRadians(lat2);
+    const long2Rad = convertToRadians(long2);
+  
+    const distance = 3963*Math.acos((Math.sin(lat1Rad)*Math.sin(lat2Rad))+(Math.cos(lat1Rad)*Math.cos(lat2Rad)*Math.cos(long2Rad-long1Rad)));
+  
+    return distance;
+  }
+
 	return (
 		<Router>
 			<div className='App'>
--- src/components/landing-page/LandingPage.js
+++ src/components/landing-page/LandingPage.js
@@ -1,9 +1,32 @@
 import { Component } from "react";
+import { Link } from "react-router-dom";
+import "./LandingPage.css";
 
 class LandingPage extends Component {
   render() {
     return (
-        <div className="main">Landing Page</div>
+      <div className="row mt-5">
+        <div className="col-6 center">
+          <h1>Be Alert.</h1>
+          <h1>Be Aware.</h1>
+          <h1>Be There.</h1>
+          <Link
+            exact="true"
+            to="/login"
+            className="btn btn-outline-secondary mt-3 mx-1"
+          >
+            Login
+          </Link>
+          <Link
+            exact="true"
+            to="/map"
+            className="btn btn-outline-secondary mt-3 mx-1"
+          >
+            Map
+          </Link>
+        </div>
+        <div className="col-6"></div>
+      </div>
     );
   }
 }
