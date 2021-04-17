6845d52fdd73f89e0dec8e8fa00422afff119cea
Faris Ashai <farisashai@gmail.com>
2021-04-17 14:51:10 -0700

    a

---
+++ src/components/map-page/Dashboard.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './Dashboard.css';
+const Dashboard = () => {
+	return <div></div>;
+};
+
+export default Dashboard;
---
+++ src/components/map-page/Map.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './Map.css';
+const Map = () => {
+	return <div></div>;
+};
+
+export default Map;
