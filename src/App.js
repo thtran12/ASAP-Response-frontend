fda2ac7a5df0c14893733d7e49411db3f812e3e4
Faris Ashai <farisashai@gmail.com>
2021-04-17 15:07:08 -0700

    a

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import "./App.css";
-import { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Header from "./components/header/Header";
-import LandingPage from "./components/landing-page/LandingPage";
-import MapPage from "./components/map-page/MapPage";
+import { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/header/Header';
+import LandingPage from './components/landing-page/LandingPage';
+import MapPage from './components/map-page/MapPage';
+import sampleData from './datasets/sampleData.json';
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <Header />
-          <Switch>
-            <Route path="/" exact render={() => <LandingPage />}></Route>
-          </Switch>
-          <Switch>
-            <Route path="/map" exact render={() => <MapPage />}></Route>
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
-}
+const App = () => {
+	const [viewport, setViewport] = useState({
+		latitude: 32.712533,
+		longitude: -117.15726,
+		zoom: 10,
+		width: '100vw',
+		height: '100vh',
+	});
+	return (
+		<Router>
+			<div className='App'>
+				<Header />
+				<Switch>
+					<Route path='/' exact render={() => <LandingPage />}></Route>
+				</Switch>
+				<Switch>
+					<Route
+						path='/map'
+						exact
+						render={() => (
+							<MapPage viewport={viewport} setViewport={setViewport} />
+						)}></Route>
+				</Switch>
+			</div>
+		</Router>
+	);
+};
 
 export default App;
--- src/components/map-page/MapPage.js
+++ src/components/map-page/MapPage.js
@@ -1,11 +1,22 @@
-import { Component } from "react";
+import React from 'react';
+import './MapPage.css';
+import Dashboard from '../map-page/Dashboard';
+import ReactMapGL, { Marker } from 'react-map-gl';
 
-class MapPage extends Component {
-  render() {
-    return (
-        <div className="main">Map Page</div>
-    );
-  }
-}
+const MapPage = ({ viewport, setViewport }) => {
+	return (
+		<div className='map-page-container'>
+			<Dashboard />
+			<div className='map-container'>
+				<ReactMapGL
+					{...viewport}
+					mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+					onViewportChange={(viewport) => {
+						setViewport(viewport);
+					}}></ReactMapGL>
+			</div>
+		</div>
+	);
+};
 
 export default MapPage;
