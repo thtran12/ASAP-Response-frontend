1b2ed6619df3510c8f48d703cc824df34c31446b
Faris Ashai <farisashai@gmail.com>
2021-04-17 18:40:56 -0700

    a

--- src/components/map-page/MapPage.js
+++ src/components/map-page/MapPage.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './MapPage.css';
 import Dashboard from '../map-page/Dashboard';
-import ReactMapGL, { Marker } from 'react-map-gl';
+import ReactMapGL, {
+	Marker,
+	NavigationControl,
+	GeolocateControl,
+	FullscreenControl,
+} from 'react-map-gl';
 import sampleData from '../../datasets/sampleData.json';
 
-const MapPage = ({ viewport, setViewport }) => {
+const fullscreenControlStyle = {
+	left: 10,
+	top: 50,
+};
+const navigationStyle = {
+	top: 80,
+	left: 0,
+	margin: 10,
+};
+const geolocateStyle = {
+	top: 0,
+	left: 0,
+	margin: 10,
+};
+const positionOptions = { enableHighAccuracy: true };
+
+const MapPage = () => {
+	const [viewport, setViewport] = useState({
+		latitude: 32.712533,
+		longitude: -117.15726,
+		zoom: 10,
+		width: '100vw',
+		height: '100vh',
+	});
+
 	return (
 		<div className='map-page-container'>
-			<Dashboard></Dashboard>
+			<Dashboard />
 			<div className='map-container'>
 				<ReactMapGL
 					{...viewport}
@@ -21,9 +50,16 @@ const MapPage = ({ viewport, setViewport }) => {
 							key={user.ID}
 							latitude={user.latitude}
 							longitude={user.longitude}>
-							<button className='person-marker'>a</button>
+							<button className='person-marker'>*</button>
 						</Marker>
 					))}
+					<GeolocateControl
+						style={geolocateStyle}
+						positionOptions={positionOptions}
+						trackUserLocation
+					/>
+					<NavigationControl showZoom style={navigationStyle} />
+					<FullscreenControl style={fullscreenControlStyle} />
 				</ReactMapGL>
 			</div>
 		</div>
