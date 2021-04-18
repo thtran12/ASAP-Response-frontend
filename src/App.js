97cbcc2e498613a088c1bf46a7e5a2ab7c90f82a
Faris Ashai <farisashai@gmail.com>
2021-04-17 21:11:08 -0700

    landing page

--- src/App.js
+++ src/App.js
@@ -1,64 +1,47 @@
-import { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Header from "./components/header/Header";
-import LandingPage from "./components/landing-page/LandingPage";
-import MapPage from "./components/map-page/MapPage";
-import LoginPage from "./components/login/LoginPage";
-import sampleData from "./datasets/sampleData.json";
-
+import { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/header/Header';
+import LandingPage from './components/landing-page/LandingPage';
+import MapPage from './components/map-page/MapPage';
+import LoginPage from './components/login/LoginPage';
+import sampleData from './datasets/sampleData.json';
+import Navbar from './components/alternative-navbar/Navbar';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      user: null,
-      viewport: {
-        latitude: 32.712533,
-        longitude: -117.15726,
-        width: "100vw",
-        height: "100vh",
-        zoom: 10,
-      },
-    };
-    this.Login = this.Login.bind(this);
-  }
+	constructor() {
+		super();
+		this.state = {
+			user: null,
+		};
+		this.handleLogin = this.handleLogin.bind(this);
+	}
 
+	handleLogin(user) {
+		console.log(user);
+		this.setState({ user });
+	}
 
-  setViewPort(viewport) {
-    this.setState({ viewport });
-  }
-
-  Login(user){
-    console.log(user);
-    this.setState({ user });
-  }
-
-  render() {
-    const { viewport } = this.state.viewport;
-
-    return (
-      <Router>
-        <div className="App">
-          <Header />
-          <Switch>
-            <Route path="/" exact render={() => <LandingPage />}></Route>
-          </Switch>
-          <Switch>
-            <Route
-              path="/map"
-              exact
-              render={() => (
-                <MapPage viewport={viewport} setViewport={this.setViewport} />
-              )}
-            ></Route>
-          </Switch>
-          <Switch>
-            <Route path="/login" exact render={() => <LoginPage onLogin={this.Login}/>}></Route>
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
+	render() {
+		return (
+			<Router>
+				<div className='App'>
+					<Navbar />
+					<Switch>
+						<Route path='/' exact render={() => <LandingPage />}></Route>
+					</Switch>
+					<Switch>
+						<Route path='/map' exact render={() => <MapPage />}></Route>
+					</Switch>
+					<Switch>
+						<Route
+							path='/login'
+							exact
+							render={() => <LoginPage onLogin={this.handleLogin} />}></Route>
+					</Switch>
+				</div>
+			</Router>
+		);
+	}
 }
 
 export default App;
